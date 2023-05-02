96d809cc861f531f8584c828cd6e782cfc29c686
Andy Cheung <andychen1401@gmail.com>
2023-05-02 19:30:55 -0400

    cleaned up files

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-// async function openTab(evt, tabName) {
-//   var i, tabContent, tabs;
-
-//   // Hide all tab content
-//   tabContent = document.getElementsByClassName("tab-content");
-//   for (i = 0; i < tabContent.length; i++) {
-//     tabContent[i].style.display = "none";
-//   }
-
-//   // Remove the background color from all tabs
-//   tabs = document.getElementsByClassName("tab");
-//   for (i = 0; i < tabs.length; i++) {
-//     tabs[i].style.backgroundColor = "";
-//   }
-
-//   // Fetch the content of the corresponding HTML file
-//   const response = await fetch(`${tabName}.html`);
-//   const content = await response.text();
-
-//   // Show the current tab and set its background color
-//   const tab = document.getElementById(tabName);
-//   tab.innerHTML = content;
-//   tab.style.display = "block";
-//   evt.currentTarget.style.backgroundColor = "#ddd";
-// }
-
-
 $(document).ready(function() {
   // Load the content of the first tab by default
   $("#practicalProblems").load("hardware.html");
