4435af356435fb57c03c627e4a8e325898e31a60
Andy Cheung <andychen1401@gmail.com>
2023-05-02 19:25:52 -0400

    updated website

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
-async function openTab(evt, tabName) {
-  var i, tabContent, tabs;
+// async function openTab(evt, tabName) {
+//   var i, tabContent, tabs;
 
-  // Hide all tab content
-  tabContent = document.getElementsByClassName("tab-content");
-  for (i = 0; i < tabContent.length; i++) {
-    tabContent[i].style.display = "none";
-  }
+//   // Hide all tab content
+//   tabContent = document.getElementsByClassName("tab-content");
+//   for (i = 0; i < tabContent.length; i++) {
+//     tabContent[i].style.display = "none";
+//   }
 
-  // Remove the background color from all tabs
-  tabs = document.getElementsByClassName("tab");
-  for (i = 0; i < tabs.length; i++) {
-    tabs[i].style.backgroundColor = "";
-  }
+//   // Remove the background color from all tabs
+//   tabs = document.getElementsByClassName("tab");
+//   for (i = 0; i < tabs.length; i++) {
+//     tabs[i].style.backgroundColor = "";
+//   }
 
-  // Fetch the content of the corresponding HTML file
-  const response = await fetch(`${tabName}.html`);
-  const content = await response.text();
+//   // Fetch the content of the corresponding HTML file
+//   const response = await fetch(`${tabName}.html`);
+//   const content = await response.text();
 
-  // Show the current tab and set its background color
-  const tab = document.getElementById(tabName);
-  tab.innerHTML = content;
-  tab.style.display = "block";
-  evt.currentTarget.style.backgroundColor = "#ddd";
-}
+//   // Show the current tab and set its background color
+//   const tab = document.getElementById(tabName);
+//   tab.innerHTML = content;
+//   tab.style.display = "block";
+//   evt.currentTarget.style.backgroundColor = "#ddd";
+// }
+
+
+$(document).ready(function() {
+  // Load the content of the first tab by default
+  $("#practicalProblems").load("hardware.html");
+
+  // When a tab is clicked, load its content
+  $(".nav-link").click(function() {
+    var tabId = $(this).attr("href");
+    $(tabId).load(tabId.substring(1) + ".html");
+  });
+});
