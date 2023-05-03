8d52b95608993874469b79f0e084e0fd2f9b8a58
Andy Cheung <andychen1401@gmail.com>
2023-05-02 20:05:39 -0400

    try try again

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
+  console.log("Custom script executed."); // Add this line
   // Load the content of the first tab by default
-  $("#practicalProblems").load("practicalProblems.html");
+  $("#problemDescription").load("problemDescription.html");
 
   // When a tab is clicked, load its content
   $(".nav-link").click(function() {
